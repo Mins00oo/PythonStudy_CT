e1c2417e604467ab559bbad078c5c2839bdd16d0
Minsu Kang <109537583+Mins00oo@users.noreply.github.com>
2022-11-29 08:41:54 +0900

    Update B5_10869_사칙연산.py

--- BACKJOON/Python/B5_10869_사칙연산.py
+++ BACKJOON/Python/B5_10869_사칙연산.py
@@ -1,4 +1,9 @@
-a, b = map(int , input().split())
+# 수도코드 작성
+# 1. 두 수를 서로 띄워서 입력받는다
+# 2. 실수형으로 변환시켜준다
+# 3. 각 줄에 해당하는 연산을 출력시킨다.
+
+a, b = map(int, input().split())
 # split의 결과를 모두 int형으로 변환시키기 위해 작성
 
 print(a+b)
