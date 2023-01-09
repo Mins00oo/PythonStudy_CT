bfca966120eaac28fb1e2327c08271e88076a33a
Minsu Kang <alstn7223@gmail.com>
2023-01-09 15:08:34 +0900

    Update S4_16173_점프왕 젤리.py

--- BACKJOON/Python/S4/S4_16173_점프왕 젤리.py
+++ BACKJOON/Python/S4/S4_16173_점프왕 젤리.py
@@ -1,29 +1,51 @@
+from collections import deque
+
 n = int(input())  # 구역의 크기
 graph = [list(map(int, input().split())) for _ in range(n)]
 dx = [1, 0]  # 아래, 오른쪽으로 움직였을때의 방향
 dy = [0, 1]
 visited = [[0] * n for _ in range(n)]
+bfs_visited = [[0] * n for _ in range(n)]
 
 
-def dfs(x, y):
-    if x >= n or x <= -1 or y >= n or y <= -1:  # 범위를 벗어나지 않도록
-        return False
-    if visited[x][y] == 1:
-        return False
+# def dfs(x, y):
+#     if x >= n or x <= -1 or y >= n or y <= -1:  # 범위를 벗어나지 않도록
+#         return False
+#     if visited[x][y] == 1:
+#         return False
+#
+#     if graph[x][y] == -1:
+#         print('HaruHaru')
+#         exit()  # 종료
+#
+#     visited[x][y] = 1
+#
+#     for i in range(2):
+#         nx = x + dx[i] * graph[x][y]
+#         ny = y + dy[i] * graph[x][y]
+#         dfs(nx, ny)
+#     return True
 
-    if graph[x][y] == -1:
-        print('HaruHaru')
-        exit()  # 종료
 
-    visited[x][y] = 1
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    bfs_visited[x][y] = 1
+    while queue:
+        x, y = queue.popleft()
+        for d in range(2):
+            nx = x + dx[d] * graph[x][y]
+            ny = y + dy[d] * graph[x][y]
+            if nx < n and ny < n and bfs_visited[nx][ny] == 0:
+                bfs_visited[nx][ny] = 1
+                queue.append((nx, ny))
+    return bfs_visited[n - 1][n - 1]
 
-    for i in range(2):
-        nx = x + dx[i] * graph[x][y]
-        ny = y + dy[i] * graph[x][y]
-        dfs(nx, ny)
-    return True
 
+# if dfs(0, 0):
+#     print('Hing')
 
-if dfs(0, 0):
+if bfs(0, 0) == 1:
+    print('HaruHaru')
+else:
     print('Hing')
-
