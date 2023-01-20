6af3da09ad489d47e19802616a6dc709c73fcfb8
Minsu Kang <alstn7223@gmail.com>
2023-01-20 17:45:49 +0900

    Add files via upload

--- BACKJOON/Python/S4/S4_1920_ 수 찾기.py
+++ BACKJOON/Python/S4/S4_1920_ 수 찾기.py
@@ -5,11 +5,31 @@ a_li = list(map(int, input().split()))
 
 M = int(input())
 b_li = list(map(int, input().split()))
+a_li.sort()  # 정렬을 해줘야 함!!
 
-c = Counter(a_li)
 
-for i in b_li:
-    if i in c:
+# c = Counter(a_li)
+# # print(c)
+# for i in b_li:
+#     if i in c:
+#         print(1)
+#     else:
+#         print(0)
+def binary(target, start, end):
+    while start <= end:
+        mid = (start + end) // 2  # 중간 인덱스 값임 !
+        if a_li[mid] == target:
+            return True
+        elif a_li[mid] > target:
+            end = mid - 1
+            return binary(target, start, end)
+        elif a_li[mid] < target:
+            start = mid + 1
+            return binary(target, start, end)
+
+
+for i in range(M):
+    if binary(b_li[i], 0, N - 1):
         print(1)
     else:
         print(0)
