fdd6e1ca5b33962d60ac51802ed32b1c925b3d5f
Minsu Kang <alstn7223@gmail.com>
2023-01-21 19:41:31 +0900

    Add files via upload

--- BACKJOON/Python/S5/S5_10815_숫자 카드.py
+++ BACKJOON/Python/S5/S5_10815_숫자 카드.py
@@ -6,10 +6,31 @@ a_li = list(map(int, input().split()))
 M = int(input())
 b_li = list(map(int, input().split()))
 
-c = Counter(a_li)
-# print(c)
-for i in b_li:
-    if i in c:
-        print(c[i], end=' ')
+a_li.sort()
+
+
+# c = Counter(a_li)
+# # print(c)
+# for i in b_li:
+#     if i in c:
+#         print(1, end=' ')
+#     else:
+#         print(0, end=' ')
+
+def binary(target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if a_li[mid] == target:
+            return True
+        elif a_li[mid] > target:
+            end = mid - 1
+        elif a_li[mid] < target:
+            start = mid + 1
+    return False
+
+
+for x in b_li:
+    if binary(x, 0, N - 1):
+        print(1, end=" ")
     else:
-        print(0, end=' ')
+        print(0, end=" ")
