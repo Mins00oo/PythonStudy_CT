495277f60f1919c0ffca4d3faf7a34ca9ee2484e
mins00oo <alstn7223@gmail.com>
2023-02-15 11:48:00 +0900

    [그래프 이론] 7562_나이트의 이동 수정

--- BACKJOON/Python/S1/S1_7562_나이트의 이동.py
+++ BACKJOON/Python/S1/S1_7562_나이트의 이동.py
@@ -6,10 +6,22 @@ dx = [-1, -2, -2, -1, 1, 2, 2, 1]
 dy = [2, 1, -1, -2, -2, -1, 1, 2]
 
 
-def bfs(a, b, c, d):
+def bfs(st_x, st_y, tar_x, tar_y):
     # a, b, c, d 순서대로 현재 x, y좌표 / 목표 x, y 좌표
-    q = deque([a, b])
-    s[a][b] = 1
+    q = deque()
+    q.append([st_x, st_y])
+    s[st_x][st_y] = 1
+    while q:
+        a, b = q.popleft()
+        if a == tar_x and b == tar_y:
+            print(s[tar_x][tar_y] - 1)
+            return
+        for d in range(8):
+            nx = a + dx[d]
+            ny = b + dy[d]
+            if 0 <= nx < i and 0 <= ny < i and s[nx][ny] == 0:
+                q.append([nx, ny])
+                s[nx][ny] = s[a][b] + 1
 
 
 for _ in range(t):
@@ -20,3 +32,4 @@ for _ in range(t):
     # 도달해야하는 좌표
     tx, ty = map(int, input().split())
     s = [[0] * i for _ in range(i)]
+    bfs(nx, ny, tx, ty)
