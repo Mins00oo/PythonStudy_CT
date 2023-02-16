2661f737ef9b5b5ef2693f1b5452dd1f7fb1d0b7
mins00oo <alstn7223@gmail.com>
2023-02-16 13:05:25 +0900

    [그래프 이론] 11403_경로 찾기 수정, 기타 문제 업로드

--- BACKJOON/Python/S1/S1_11403_경로 찾기.py
+++ BACKJOON/Python/S1/S1_11403_경로 찾기.py
@@ -5,4 +5,22 @@ graph = []
 # 좌표 입력받기
 for _ in range(n):
     graph.append(list(map(int, input().split())))
+visited = [0 * n for _ in range(n)]
 
+
+def dfs(x):
+    for i in range(n):
+        if graph[x][i] == 1 and visited[i] == 0:
+            visited[i] = 1
+            dfs(i)
+
+
+for m in range(n):
+    dfs(m)
+    for j in range(n):
+        if visited[j] == 1:
+            print(1, end=' ')
+        else:
+            print(0, end=' ')
+    print()
+    visited = [0 for _ in range(n)]
---
+++ 기타 문제/3학년 2반.py
@@ -0,0 +1,13 @@
+# 학생 수
+n = int(input())
+student = []
+iq = []
+for _ in range(n):
+    a, b = map(str, input().split())
+    student.append([a, b])
+
+student.sort(reverse=True, key=lambda s: int(s[1]))
+
+print(student[0][0])
+print(student[1][0])
+print(student[2][0])
