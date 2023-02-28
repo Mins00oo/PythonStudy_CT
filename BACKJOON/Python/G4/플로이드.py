11c8e7a7013a66e95b4843f156a1bd3ecb136990
mins00oo <alstn7223@gmail.com>
2023-02-28 17:50:57 +0900

    FEAT: 백준_G4_플로이드 코드 업로드
    단순한 플로이드 와샬 알고리즘 문제로 처음 리스트에 잘 담아둔 다음, for문안에서 최적의 경로로 바꿔서 나중에 출력해준다
    
    해결: #1

---
+++ BACKJOON/Python/G4/플로이드.py
@@ -0,0 +1,24 @@
+# 도시의 개수
+n = int(input())
+# 버스의 개수
+m = int(input())
+INF = 1000000000
+graph = [[INF] * n for _ in range(n)]
+for i in range(m):
+    s, e, c = map(int, input().split())
+    if graph[s - 1][e - 1] > c:
+        graph[s - 1][e - 1] = c
+
+# 거쳐가야 하는 노드
+for k in range(n):
+    # 출발점
+    for a in range(n):
+        # 도착점
+        for b in range(n):
+            if a == b:
+                graph[a][b] = 0
+            if graph[a][b] > graph[a][k] + graph[k][b] and a != b:
+                graph[a][b] = graph[a][k] + graph[k][b]
+
+for i in graph:
+    print(*i)
---
+++ Programmers/Lv1/콜라.py
@@ -0,0 +1,4 @@
+def solution(a, b, n):
+    answer = 0
+
+    return answer
