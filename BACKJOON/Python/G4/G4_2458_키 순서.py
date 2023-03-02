f71a4a8558fa581ebf1fcb4880ebb21e542816e2
mins00oo <alstn7223@gmail.com>
2023-03-02 17:04:49 +0900

    REFACTOR: 프로그래머스_Lv1_콜라 업로드
    전체 병의 개수 n이 주는 병의 개수를 나누어 떨어지는 경우와 그렇지 않은 경우로 나누어서 문제 해결
    
    해결: #8

--- BACKJOON/Python/G4/G4_2458_키 순서.py
+++ BACKJOON/Python/G4/G4_2458_키 순서.py
@@ -1,4 +1,7 @@
 # 학생들의 수, 키를 비교한 횟수
+import sys
+
+input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [[0 for _ in range(n)] for _ in range(n)]
 
@@ -6,12 +9,18 @@ for i in range(m):
     a, b = map(int, input().split())
     graph[a - 1][b - 1] = 1
 
-
 for k in range(n):
     for i in range(n):
         for j in range(n):
             if graph[i][k] == 1 and graph[k][j] == 1:
                 graph[i][j] = 1
 
+answer = 0
+for i in range(n):
+    result = 0
+    for j in range(n):
+        result += graph[i - 1][j - 1] + graph[j - 1][i - 1]
+    if result == n - 1:
+        answer += 1
 
-print(graph)
+print(answer)
--- Programmers/Lv1/콜라.py
+++ Programmers/Lv1/콜라.py
@@ -1,4 +1,14 @@
+# a는 줘야하는 병의 개수, b는 받는 개수, n은 처음 보유한 개수
 def solution(a, b, n):
     answer = 0
-
+    while n >= a:
+        if n % a == 0:
+            answer += (n // a) * b
+            n = (n // a) * b
+        else:
+            answer += (n // a) * b
+            n = ((n - (n % a)) // a) * b + (n % a)
     return answer
+
+
+print(solution(2, 1, 20))
