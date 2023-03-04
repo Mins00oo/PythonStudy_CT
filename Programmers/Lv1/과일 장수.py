f909e56c8362d13716c2940fbac607727016fce8
mins00oo <alstn7223@gmail.com>
2023-03-04 14:39:22 +0900

    REFACTOR: 프로그래머스_Lv1_과일장수 코드 수정
    상세 주석을 조금 더 추가
    
    해결: #8

--- Programmers/Lv1/과일 장수.py
+++ Programmers/Lv1/과일 장수.py
@@ -1,10 +1,11 @@
 def solution(k, m, score):
     answer = 0
-    # 박스의 개수 -> 1개
+    # 내림차순 정렬
     score.sort(reverse=True)
     for i in range(0, len(score), m):
-        tmp = score[i:i+m]
-
+        # m개만큼의 tmp 생성
+        tmp = score[i:i + m]
+        # tmp의 길이가 담기는 과일 개수인 m과 같으면 answer에 추가
         if len(tmp) == m:
             answer += min(tmp) * m
     return answer
