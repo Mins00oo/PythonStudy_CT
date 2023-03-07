d74c527a33809916b440a36e4c71015dcbd8690d
mins00oo <alstn7223@gmail.com>
2023-03-07 14:57:59 +0900

    FEAT: 백준_s2_11724연결 요소의 개수
    dfs와 bfs로 모두 풀어보고 다시 업로드
    
    해결: #6

--- BACKJOON/Python/S2/S2_11724_연결 요소의 개수.py
+++ BACKJOON/Python/S2/S2_11724_연결 요소의 개수.py
@@ -55,6 +55,8 @@
 # 2차 시간 초과 발생
 
 import sys
+from collections import deque
+
 sys.setrecursionlimit(10 ** 6)
 
 n, m = map(int, sys.stdin.readline().split())  # n은 노드의 개수, m은 서로 연결된 간선의 개수
@@ -65,21 +67,34 @@ for _ in range(m):
     graph[a].append(b)
     graph[b].append(a)
 
-visited = [0] * (n + 1)
+dfs_visited = [0] * (n + 1)
+bfs_visited = [0] * (n + 1)
 cnt = 0
 
 
 def dfs(start):
-    global cnt
-    visited[start] = 1
-    for d in graph[start]:
-        if visited[d] == 0:
-            dfs(d)
+    dfs_visited[start] = 1
+    for i in graph[start]:
+        if dfs_visited[i] == 0:
+            dfs(i)
 
 
-for i in range(1, n + 1):
-    if visited[i] == 0:
+def bfs(start):
+    bfs_visited[start] = 1
+    q = deque()
+    q.append(start)
+
+    while q:
+        d = q.popleft()
+        for i in graph[d]:
+            if bfs_visited[i] == 0:
+                bfs_visited[i] = 1
+                q.append(i)
+
+
+for j in range(1, n + 1):
+    if bfs_visited[j] == 0:
         cnt += 1
-        dfs(i)
+        bfs(j)
 
 print(cnt)
