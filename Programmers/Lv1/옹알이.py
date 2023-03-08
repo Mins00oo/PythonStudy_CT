7eb69cca847d1e5ff31dfcfa272353bdf38e0e57
mins00oo <alstn7223@gmail.com>
2023-03-08 20:54:53 +0900

    FEAT: 프로그래머스_Lv1_옹알이 업로드
    발음할 수 있는 단어를 기반으로 이중 for문을 통해 연속되어 있는지 확인하고 그렇다면 패스, 그렇지 않고 해당하는 발음할 수 있다면 빈 문자열로 바꿔주면서 전부 빈 문자열인 발음들을 count해준다.
    
    해결: #8

--- Programmers/Lv1/옹알이.py
+++ Programmers/Lv1/옹알이.py
@@ -1,3 +1,17 @@
 def solution(babbling):
-    answer = 0
-    return answer
+    # 발음할 수 있는 단어
+    speak = ["aya", "ye", "woo", "ma"]
+    # 말할 수 있는지 판단하기 위해
+    for i in range(len(babbling)):
+        for j in range(len(speak)):
+            # 말할 수 있는 단어와 연결되어 있다면 발음하지 못하기에 continue
+            if speak[j] + speak[j] in babbling[i]:
+                continue
+            # 말할 수 있는 단어가 있다면 그 부분은 빈 문자열로 바꿔주고 for문을 계속 실행
+            # 문자열 자체가 이렇게 빈 문자열로 된다면 발음이 된다는 뜻
+            if speak[j] in babbling[i]:
+                babbling[i] = babbling[i].replace(speak[j], "")
+    return babbling.count("")
+
+
+print(solution(["aya", "yee", "u", "maa"]))
