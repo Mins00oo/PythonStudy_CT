732f63ed5bd417003537cebbdb6b0cf258b03195
mins00oo <alstn7223@gmail.com>
2023-03-14 11:38:00 +0900

    FEAT: 백준_G2_K번째 수
    이분탐색 알고리즘 문제로, 시작점과 마지막점을 중간값으로 조절해나가면서 풀면 된다. 단, 이중 for문으로 리스트안에 값을 넣고 다시 B 배열을 정렬하면 메모리 초과가 발생하고 좋지 않은 방법! 따라서, 이분탐색을 활용해서 문제를 풀도록
    
    해결: #4

---
+++ BACKJOON/Python/G2/K번째 수.py
@@ -0,0 +1,19 @@
+# 배열의 크기 n, k번째 인덱스
+n, k = int(input()), int(input())
+start, end = 1, k
+answer = 0
+
+while start <= end:
+    mid = (start + end) // 2
+
+    temp = 0
+    for i in range(1, n + 1):
+        temp += min(mid // i, n)
+
+    if temp >= k:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(answer)
