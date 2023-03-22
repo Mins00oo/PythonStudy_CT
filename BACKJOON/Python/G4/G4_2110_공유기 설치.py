7a69ae18bb78d4da98eca20bf72574b840ebd171
mins00oo <alstn7223@gmail.com>
2023-03-22 11:32:16 +0900

    FEAT: 코테 스터디 문제 미리 업로드만
    문제 형식만 만들어서 올림
    
    관련: #3

--- BACKJOON/Python/G4/G4_2110_공유기 설치.py
+++ BACKJOON/Python/G4/G4_2110_공유기 설치.py
@@ -1,18 +1,20 @@
+# n은 집, c는 공유기의 수
 n, c = map(int, input().split())
-n_li = [int(input()) for _ in range(n)]
+home_li = sorted([int(input()) for _ in range(n)])
 answer = 0
-n_li.sort()
 
 
 def binary(start, end):
     while start <= end:
         mid = (start + end) // 2
-        now = n_li[0]
+        now = home_li[0]
+        # 맨 앞집부터 공유기 설치, 그 다음부터는 거리에 따라서
         count = 1
         global answer
-        for i in range(1, len(n_li)):
-            if n_li[i] >= mid + now:
-                now = n_li[i]
+        # 맨 앞집 다음부터 어느 집에 공유기를 설치할 수 있는지
+        for i in range(1, len(home_li)):
+            if home_li[i] >= mid + now:
+                now = home_li[i]
                 count += 1
         # 공유기가 mid 거리로 설치가 된다면 최대 거리 구하고
         if count >= c:
@@ -23,5 +25,5 @@ def binary(start, end):
             end = mid - 1
 
 
-binary(1, max(n_li) - n_li[0])
+binary(1, max(home_li) - home_li[0])
 print(answer)
--- BACKJOON/Python/G5/G5_1756_피자 굽기.py
+++ BACKJOON/Python/G5/G5_1756_피자 굽기.py
@@ -1,24 +1,24 @@
 # 오븐의 깊이 d, 피자 반죽의 개수 n
 d, n = map(int, input().split())
-d_list = list(map(int, input().split()))
-n_list = list(map(int, input().split()))
+oven_list = list(map(int, input().split()))
+pizza_list = list(map(int, input().split()))
 
 # 어차피 앞에서 통과를 못하면 안으로 피자가 못 들어온다. 그래서 리스트를 조금 수정
-for i in range(1, len(d_list)):
-    if d_list[i] > d_list[i - 1]:
-        d_list[i] = d_list[i - 1]
+for i in range(1, len(oven_list)):
+    if oven_list[i] > oven_list[i - 1]:
+        oven_list[i] = oven_list[i - 1]
 
 # 맨 밑에서부터 탐색
 # 오븐에 들어간 피자의 개수
-n_count = 0
+pizza_count = 0
 for i in range(d - 1, -1, -1):
-    if n_list[n_count] <= d_list[i]:
-        n_count += 1
+    if pizza_list[pizza_count] <= oven_list[i]:
+        pizza_count += 1
 
-    if n_count == n:
+    if pizza_count == n:
         print(i + 1)
         break
 
 # 들어간 피자의 수가 전체 피자 수랑 다르다 즉, 모든 피자가 들어가지 않았다면
-if n_count != n:
+if pizza_count != n:
     print(0)
