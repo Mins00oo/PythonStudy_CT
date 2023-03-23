797b6a0e271df920b1b2e5953aefabda29187076
mins00oo <alstn7223@gmail.com>
2023-03-23 16:56:52 +0900

    FEAT: S1_1932_정수 삼각형.py
    DP문제로, 삼각형을 생각했을때 위에서 아래로 더해가면서 안의 수를 바꿔준다. 삼각형은 이차원 배열로 나타낼 수 있기에 인덱스 값을 헷갈리지 않으면서 값을 계속 바꿔나가고 마지막 줄의 최댓값 출력하면 됨
    
    해결: #3

---
+++ BACKJOON/Python/S1/S1_1932_정수 삼각형.py
@@ -0,0 +1,18 @@
+n = int(input())
+t = []
+for i in range(n):
+    t.append(list(map(int, input().split())))
+
+k = 2
+for i in range(1, n):
+    for j in range(k):
+        # 양 끝에 있는 점인지를 먼저 확인하는 두 조건문
+        if j == 0:
+            t[i][j] += t[i - 1][j]
+        elif i == j:
+            t[i][j] += t[i - 1][j - 1]
+        else:
+            t[i][j] += max(t[i - 1][j], t[i - 1][j - 1])
+    k += 1
+
+print(max(t[n - 1]))
