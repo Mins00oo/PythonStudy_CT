10cdb4aeedf9bc3444ef8370cf646d98dab89e87
mins00oo <alstn7223@gmail.com>
2023-03-30 19:11:13 +0900

    FEAT: S3_1966_프린터 큐.py
    백준 구현 알고리즘 문제로 아이디어는 맨 앞의 값이 리스트에서 최대가 되기 전까지 FIFO를 반복한다. 하지만, 최댓값이 맨 앞에 오더라도 우리가 알아야 할 m에 해당했던 값인지 아닌지 체크를 해야하기에 숫자 n개만큼의 인덱스 길이를 만들어놓고 바꾼다.
    
    해결: #8

--- BACKJOON/Python/S3/S3_1966_프린터 큐.py
+++ BACKJOON/Python/S3/S3_1966_프린터 큐.py
@@ -2,3 +2,19 @@ t = int(input())
 
 for _ in range(t):
     n, m = map(int, input().split())
+    p = list(map(int, input().split()))
+    index = [i for i in range(n)]
+    index[m] = -1
+    cnt = 0
+
+    while p:
+        if p[0] == max(p):
+            cnt += 1
+            if index[0] == -1:
+                print(cnt)
+                break
+            p.pop(0)
+            index.pop(0)
+        else:
+            p.append(p.pop(0))
+            index.append(index.pop(0))
