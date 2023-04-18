2e74bfbc72e163ba33c922930e7af677d80293e7
mins00oo <alstn7223@gmail.com>
2023-04-18 12:18:50 +0900

    FEAT: G5_7569_토마토.py
    6가지 인접한 좌표들로 탐색을 하며, 조건에 충족할 시 값을 바꿔주며 계속해서 반복한다. 이를 위해, BFS알고리즘을 사용하였고 마지막에는 익지 않은 토마토가 있는지 확인하여 -1을 리턴한다. 3차원 배열을 생성하는데 무리가 없다면 풀 수 있는 문제다.
    
    해결: #6

--- BACKJOON/Python/G5/G5_7569_토마토.py
+++ BACKJOON/Python/G5/G5_7569_토마토.py
@@ -1,8 +1,46 @@
 """수도코드
 1. 입력받기: 행을 나타내는 n, 열을 나타내는 m, 높이를 나타내는 h / 토마토에 대한 정보는 h만큼 반복해서 받는다.
 2. h만큼 graph를 만들어놓고 입력값은 위에서부터 순서대로 주어지니깐 순서대로 graph에 입력값을 넣는걸로
-3.
-
+3. bfs 함수 정의
+3.1 박스안에서 익은 토마토가 존재하는 좌표라면 queue에 넣어준다. -> 이 익은 토마토의 좌표를 통해 퍼트리는거기 때문에
+3.2 다음으로 갈 방향으로 앞전에 설정해놓은 방향 6개를 통해 잡아주고 범위안에 속하며 그 좌표값의 토마토 상태가 익지 않았는지 확인한다
+3.3 조건에 만족하면 해당 좌표값을 익었다고 즉 ,1로 바꿔주고 queue에 넣어주고 출력해야하는 총 일수에 걸린 횟수 + 1을 해준다.
+3.4 while문을 전부 돌아서 토마토 상태를 바꿨지만 아직 토마토 상태가 안 익은게 있다면 -1을 리턴한다.
 """
-n, m, h = map(int, input().split())
-graph = [[0 for _ in range(m)] for _ in range(n)]
+from collections import deque
+
+
+def solution():
+    queue = deque()
+    for i in range(h):
+        for j in range(n):
+            for k in range(m):
+                # 익은 토마토가 있는 좌표라면
+                if graph[i][j][k] == 1:
+                    queue.append([i, j, k, 0])
+    min_day = 0
+    while queue:
+        x, y, z, day = queue.popleft()
+        for dx, dy, dz in direction:
+            nx = x + dx
+            ny = y + dy
+            nz = z + dz
+            if 0 <= nx < h and 0 <= ny < n and 0 <= nz < m and graph[nx][ny][nz] == 0:
+                graph[nx][ny][nz] = 1
+                queue.append([nx, ny, nz, day + 1])
+                min_day = day + 1
+
+    for i in range(h):
+        for j in range(n):
+            for k in range(m):
+                # 익은 토마토가 있는 좌표라면
+                if graph[i][j][k] == 0:
+                    return -1
+
+    return min_day
+
+
+m, n, h = map(int, input().split())
+graph = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+direction = [(1, 0, 0), (-1, 0, 0), (0, 0, -1), (0, 0, 1), (0, -1, 0), (0, 1, 0)]
+print(solution())
