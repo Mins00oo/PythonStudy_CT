58d17d1ce98e3dcc0c37bfeca1de6d38f99fff72
mins00oo <alstn7223@gmail.com>
2023-05-23 11:28:28 +0900

    FEAT: 둘만의 암호.py
    전체 알파벳을 문자열로 선언한다. 스킵해야하는 문자열을 먼저 알파벳에서 제외하고 답을 구하기 위해 s문자열에서 index만큼 늘리는데 최대 길이를 초과할 수 있다. 따라서, 최대 길이만큼 나눠준다.
    
    해결: #10

--- Programmers/Lv1/둘만의 암호.py
+++ Programmers/Lv1/둘만의 암호.py
@@ -1,18 +1,21 @@
+"""수도코드
+1. 입력받기
+2. 함수정의
+2.1 알파벳 전체를 문자열로 선언해준다.
+2.2 스킵해야하는 문자는 알파벳 전체에서 제외해준다
+2.3 index만큼 늘려야하는 s를 반복하면서 answer에 추가해준다.
+"""
+
+
 def solution(s, skip, index):
     answer = ''
-    alpha_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    for i in skip:
-        alpha_list.remove(i)
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+
+    for sk in skip:
+        alpha = alpha.replace(sk, "")
+
     for i in s:
-        now = alpha_list.index(i)
-        d = now + index
-        length = len(alpha_list)
-        if d >= length:
-            dn = (now + index) - length
-            answer += alpha_list[dn]
-        else:
-            answer += alpha_list[d]
+        answer += alpha[(alpha.index(i) + index) % len(alpha)]
 
     return answer
 
