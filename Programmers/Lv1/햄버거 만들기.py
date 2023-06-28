a9f4db4b09331c24402b17be4a349a13f576f124
mins00oo <alstn7223@gmail.com>
2023-06-28 12:35:06 +0900

    FEAT: 햄버거 만들기.py
    리스트에서 뒤에서부터 인덱싱을 활용하는 법을 안다면 풀 수 있는 문제다.
    순서대로 값을 입력받는 도중 뒤에서부터 인덱싱을 하면서 값이 존재한다면 개수 + 1을 하고 값을 pop을 통해 제거
    
    해결: #10

--- Programmers/Lv1/햄버거 만들기.py
+++ Programmers/Lv1/햄버거 만들기.py
@@ -1,10 +1,20 @@
 """수도코드
-1.
+1. 재료를 순서대로 들어오게 만든다.
+2. 들어오는 재료가 1, 2, 3, 1 순서대로 있다면 개수 + 1을 하고
+3. 만드는데 사용된 재료 4개는 지우고 계속 추가한다.
 """
 
 
 def solution(ingredient):
     answer = 0
+    q = []
+    for _ in ingredient:
+        q.append(_)
+        if q[-4:] == [1, 2, 3, 1]:
+            answer += 1
+            for _ in range(4):
+                q.pop()
+
     return answer
 
 
