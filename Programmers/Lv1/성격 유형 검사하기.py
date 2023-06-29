e99306d0272364a631a910f998ca11f26c3e2e85
mins00oo <alstn7223@gmail.com>
2023-06-29 22:59:02 +0900

    FEAT: 성격 유형 검사하기.py
    각 유형에 대한 점수를 기록하기 위해 dic함수를 활용했다. 그리고 survey와 choices가 같은 길이이고 동시에 가져와야 했어서 zip함수를 통해 반복문을 돌린다.
    이후로는 간단하게 구현문제로 dic에서 값을 가져오면서 점수를 추가해주고 반복문을 통해 값을 비교해보면서 answer에 성격을 결정해주면 된다.
    
    해결: #10

--- Programmers/Lv1/성격 유형 검사하기.py
+++ Programmers/Lv1/성격 유형 검사하기.py
@@ -1,11 +1,28 @@
 """수도코드
-1.
+1. 점수가 3, 2, 1, 0, 1, 2, 3 순서대로니깐 이 순서대로 점수를 매겨본다
+2. choice값이 중간값 기준으로 적으면 첫번째 알파벳, 크면 두번째 알파벳이 점수를 획득한다. -> 3을 기준으로 잡으면 될듯
+3. 점수를 기록을 다 하고 지표를 따라서 측정해보는데 같으면 알파벳 순서대로 결정한다.
 """
 
 
 def solution(survey, choices):
     answer = ''
-    return answer
+    dic = {"R": 0, "T": 0, "C": 0, "F": 0, "J": 0, "M": 0, "A": 0, "N": 0}
+
+    for s, c in zip(survey, choices):
+        if c > 4:
+            dic[s[1]] += c - 4
+        elif c < 4:
+            dic[s[0]] += 4 - c
+
+    l = list(dic.items())
+
+    for i in range(0, 8, 2):
+        if l[i][1] < l[i + 1][1]:
+            answer += l[i + 1][0]
+        else:
+            answer += l[i][0]
+    print(answer)
 
 
-solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])
+solution(["TR", "RT", "TR"], [7, 1, 3])
